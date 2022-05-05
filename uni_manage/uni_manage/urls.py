f2c3042102b1bf6b3e3d184767c01b63de5d499f
Sumitosh <sith2594@gmail.com>
2022-05-05 09:37:20 +0530

    Index template in demo page utilizing adminLTE

--- uni_manage/uni_manage/urls.py
+++ uni_manage/uni_manage/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+from uni_manage_app import views
+from uni_manage import settings
+
 urlpatterns = [
+    path('demo',views.showDemoPage),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
